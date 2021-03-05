aedcf2af761f7403d91dfcdd24d52e26c56633a7
Arthur <arthur@arthurvardevanyan.com>
2021-03-04 21:47:23 -0500

    Mock MongoDB Server & Delete Tests

---
+++ tests/mockDB.js
@@ -0,0 +1,18 @@
+// https://stackoverflow.com/a/65002874
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+
+const mongoDB = new MongoMemoryServer();
+mongoose.connection.close();
+
+module.exports.connect = async () => {
+  const uri = await mongoDB.getUri();
+
+  const mongooseOpts = {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  };
+
+  await mongoose.connect(uri, mongooseOpts);
+};
--- tests/test.js
+++ tests/test.js
@@ -6,6 +6,10 @@ chai.use(chaiHttp);
 const app = require('../server');
 // const Product = require('../models/product.model');
 
+const mockDB = require('./mockDB');
+
+before(async () => mockDB.connect());
+
 describe('GET /products', () => {
   it('should return status 200', async () => {
     const res = await chai.request(app).get('/products').send();
@@ -22,7 +26,28 @@ describe('DELETE /products', () => {
 
 describe('POST /products', () => {
   it('should return status 201', async () => {
-    const res = await chai.request(app).post('/products').send({ sku: 3, name: 'test2', price: 2 });
+    const res = await chai.request(app).post('/products').send({ sku: 0, name: 'someItem', price: 125 });
     expect(res.status).to.equal(201);
+  });
+});
+
+describe('/products/0', () => {
+  it('should return status 200 - GET Product 0', async () => {
+    const res = await chai.request(app).get('/products/0').send();
+    expect(res.status).to.equal(200);
+  });
+  it('should return JSON Object of the item', async () => {
+    const res = await chai.request(app).get('/products/0').send();
+    expect(res.body).to.deep.equal({
+      quantity: 0, sku: 0, name: 'someItem', price: 1.25,
+    });
+  });
+  it('should return status 200 - Deleting Existing Item 0', async () => {
+    const res = await chai.request(app).delete('/products/0').send();
+    expect(res.status).to.equal(200);
+  });
+  it('should return status 404 - Deleting NON-Existing Item 0', async () => {
+    const res = await chai.request(app).delete('/products/0').send();
+    expect(res.status).to.equal(404);
   });
 });
