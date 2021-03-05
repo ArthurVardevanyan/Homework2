e31473cfb5bae3d9d5cc69a785c094210b3e3288
Arthur <arthur@arthurvardevanyan.com>
2021-03-05 13:35:50 -0500

    SSN & Phone Number Validator

--- models/user.model.js
+++ models/user.model.js
@@ -7,6 +7,11 @@ module.exports = Mongoose.model('User', new Mongoose.Schema({
     unique: true,
     min: 11,
     max: 11,
+    validate: {
+      validator(value) {
+        return /\d{3}-\d{2}-\d{4}/.test(value);
+      },
+    },
   },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
@@ -16,7 +21,15 @@ module.exports = Mongoose.model('User', new Mongoose.Schema({
     min: 0,
   },
   address: { type: String, required: false },
-  phone: { type: String, required: false },
+  phone: {
+    type: String,
+    required: false,
+    validate: {
+      validator(value) { // https://stackoverflow.com/questions/16699007/regular-expression-to-match-standard-10-digit-phone-number
+        return /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/.test(value);
+      },
+    },
+  },
 }, {
   toJSON: {
     getters: true,
