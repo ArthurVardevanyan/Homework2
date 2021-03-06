375a51b3e6fe7bbffa558dcf67d2e16b8fd7d3be
Arthur <arthur@arthurvardevanyan.com>
2021-03-06 09:19:09 -0500

    Simple Query Parameter Test

--- tests/integration/product.integration.test.js
+++ tests/integration/product.integration.test.js
@@ -80,3 +80,42 @@ describe('Patch /products/0', () => {
     });
   });
 });
+
+describe('DELETE All /products', () => {
+  it('should return status 404', async () => {
+    const res = await chai.request(app).delete('/products').send();
+    expect(res.status).to.equal(404);
+  });
+});
+
+describe('POST Multiple Products /products', () => {
+  it('should return status 201', async () => {
+    let res = await chai.request(app).post('/products').send({ sku: 0, name: 'someItem1', price: 100 });
+    expect(res.status).to.equal(201);
+    res = await chai.request(app).post('/products').send({ sku: 1, name: 'someItem1', price: 125 });
+    expect(res.status).to.equal(201);
+    res = await chai.request(app).post('/products').send({ sku: 2, name: 'someItem2', price: 125 });
+    expect(res.status).to.equal(201);
+    res = await chai.request(app).post('/products').send({ sku: 3, name: 'someItem2', price: 130 });
+    expect(res.status).to.equal(201);
+    res = await chai.request(app).post('/products').send({ sku: 4, name: 'someItem3', price: 130 });
+    expect(res.status).to.equal(201);
+  });
+});
+
+describe('/products?price=1.30 Return all Products with Price of 130', () => {
+  it('should return status 200 - GET Products with price of 130', async () => {
+    const res = await chai.request(app).get('/products?price=1.30').send();
+    expect(res.status).to.equal(200);
+  });
+  it('should return JSON Object of the item(s)', async () => {
+    const res = await chai.request(app).get('/products?price=1.30').send();
+    expect(res.body).to.deep.equal([
+      {
+        sku: 3, name: 'someItem2', price: 1.30, quantity: 0,
+      },
+      {
+        sku: 4, name: 'someItem3', price: 1.30, quantity: 0,
+      }]);
+  });
+});
--- tests/integration/user.integration.test.js
+++ tests/integration/user.integration.test.js
@@ -86,3 +86,52 @@ describe('Patch /users/000-00-0000', () => {
     });
   });
 });
+
+describe('DELETE All /users', () => {
+  it('should return status 404', async () => {
+    const res = await chai.request(app).delete('/users').send();
+    expect(res.status).to.equal(404);
+  });
+});
+
+describe('POST Multiple users /users', () => {
+  it('should return status 201', async () => {
+    let res = await chai.request(app).post('/users').send({
+      ssn: '000-00-0000', firstName: 'newFirstName', lastName: 'newLastName', age: 1,
+    });
+    expect(res.status).to.equal(201);
+    res = await chai.request(app).post('/users').send({
+      ssn: '000-00-0001', firstName: 'newFirstName', lastName: 'newLastName', age: 2,
+    });
+    expect(res.status).to.equal(201);
+    res = await chai.request(app).post('/users').send({
+      ssn: '000-00-0002', firstName: 'newFirstName', lastName: 'newLastName', age: 2,
+    });
+    expect(res.status).to.equal(201);
+    res = await chai.request(app).post('/users').send({
+      ssn: '000-00-0003', firstName: 'newFirstName', lastName: 'newLastName', age: 3,
+    });
+    expect(res.status).to.equal(201);
+    res = await chai.request(app).post('/users').send({
+      ssn: '000-00-0004', firstName: 'newFirstName', lastName: 'newLastName', age: 3,
+    });
+    expect(res.status).to.equal(201);
+  });
+});
+
+describe('/users?age=2 Return all users with age of 2', () => {
+  it('should return status 200 - GET users with age of 2', async () => {
+    const res = await chai.request(app).get('/users?age=2').send();
+    expect(res.status).to.equal(200);
+  });
+  it('should return JSON Object of the item(s)', async () => {
+    const res = await chai.request(app).get('/users?age=2').send();
+    expect(res.body).to.deep.equal([
+      {
+        ssn: '000-00-0001', firstName: 'newFirstName', lastName: 'newLastName', age: 2,
+      },
+      {
+        ssn: '000-00-0002', firstName: 'newFirstName', lastName: 'newLastName', age: 2,
+      }]);
+  });
+});
